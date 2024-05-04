d548e4cbda1b08a749e11a70d07c156cd8e4c9de
Federico Genaro <fedegen14@gmail.com>
2024-05-04 15:50:08 -0300

    Refactorización de código para mayor legibilidad

--- segundo_problema/coloreo_sin_PL.py
+++ segundo_problema/coloreo_sin_PL.py
@@ -1,6 +1,18 @@
 import networkx as nx
 from utilidades import *
 
+GRAFO = 0
+T_LAV_PRENDAS = 1
+
+MEJOR_IND_CROMATICO = 0
+MEJOR_SOL = 1
+MEJOR_TIEMPO = 2
+
+PINTADOS = 0
+T_LAVADOS = 1
+CORRECTO = 2
+T_TOTAL_LAVADOS = 3
+
 
 def es_compatible(grafo, colores, v):
     for w in grafo.neighbors(v):
@@ -9,23 +21,12 @@ def es_compatible(grafo, colores, v):
     return True
 
 
-def usa_mas_colores(pintados, cant_minima):
-    colores_usados = {}
-    for v in pintados:
-        colores_usados[pintados[v]] = colores_usados.get(pintados[v], 0) + 1
-    return len(colores_usados) > cant_minima
+def usa_mas_tiempo_lavado(tiempo_lavados, min_tiempo):
+    return obtener_tiempo_lavado(tiempo_lavados) > min_tiempo
 
 
-def usa_mas_tiempo_lavado(v_pintados, min_tiempo, tiempo_lavados):
-    return obtener_tiempo_lavado(v_pintados, tiempo_lavados) > min_tiempo
-
-
-def obtener_tiempo_lavado(prendas_lavados, tiempo_lavado):
-    tiempo_por_lavado = {}
-    for prenda in prendas_lavados:
-        lavado = prendas_lavados[prenda]
-        tiempo_por_lavado[lavado] = max(tiempo_por_lavado.get(lavado, 0), tiempo_lavado[prenda])
-    return sum(tiempo_por_lavado.values())
+def obtener_tiempo_lavado(tiempo_lavados):
+    return sum(tiempo_lavados.values())
 
 
 def crear_grafo(cant_prendas, incompatibilidades):
@@ -40,78 +41,96 @@ def crear_grafo(cant_prendas, incompatibilidades):
     return grafo
 
 
-def solucion_por_coloreo(tiempo_de_lavado):
-    min_colores_ant, min_sol_ant, min_tiempo_ant = prendas + 1, {}, sum(d_lavados.values())
+def armado_lavados(mejor_sol):
+    lavados = {}
+    for p in mejor_sol:
+        lavado_actual = mejor_sol[p]
+        lavados[lavado_actual] = lavados.get(lavado_actual, []) + [p]
+    return lavados
 
-    mejor_ind_cromatico, mejor_sol, mejor_tiempo = min_colores_ant, min_sol_ant, min_tiempo_ant
 
-    for prenda_act in range(1, prendas + 1):
+def solucion_por_coloreo(cant_prendas, t_lavado_prendas):
+    prendas, cant_colores_inicial = range(1, cant_prendas + 1), cant_prendas + 1
 
-        min_colores_act, min_sol_act, min_tiempo_act, _ = coloreo_rec(grafo_prendas, prendas,
-                                                                      {}, prenda_act,
-                                                                      min_colores_ant, min_sol_ant,
-                                                                      min_tiempo_ant, d_lavados)
+    mejor_ind_cromatico, mejor_sol, mejor_tiempo = cant_colores_inicial, {}, sum(t_lavado_prendas.values())
+
+    dominio = [grafo_prendas, t_lavado_prendas]
+    sol_optima = [mejor_ind_cromatico, mejor_sol, mejor_tiempo]
+
+    for prenda_act in prendas:
+        vertice_inicial = prenda_act
+        sol_parcial = [{}, {}, True, 0]
+
+        sol_parcial = coloreo_rec(dominio, sol_parcial, vertice_inicial, sol_optima)
+
+        if not sol_parcial[CORRECTO]:
+            continue
 
         # Agrego esto para los vertices (prendas) aislados
-        for prenda in range(1, prendas + 1):
-            if prenda not in min_sol_act:
-                min_colores_completo, min_sol_completo, min_tiempo_completo, _ = coloreo_rec(grafo_prendas, prendas,
-                                                                                             min_sol_act, prenda,
-                                                                                             min_colores_ant,
-                                                                                             min_sol_ant,
-                                                                                             min_tiempo_ant, d_lavados)
+        for prenda in prendas:
+            if prenda not in sol_parcial[PINTADOS]:
+                vertice_inicial = prenda
+                sol_parcial = coloreo_rec(dominio, sol_parcial, vertice_inicial, sol_optima)
 
-                min_colores_act, min_sol_act, min_tiempo_act = min_colores_completo, min_sol_completo, min_tiempo_completo
+        if sol_parcial[T_TOTAL_LAVADOS] < sol_optima[MEJOR_TIEMPO]:
+            sol_optima[MEJOR_IND_CROMATICO] = len(sol_parcial[T_LAVADOS])
+            sol_optima[MEJOR_SOL] = sol_parcial[PINTADOS].copy()
+            sol_optima[MEJOR_TIEMPO] = sol_parcial[T_TOTAL_LAVADOS]
 
-        if min_tiempo_act < mejor_tiempo:
-            mejor_ind_cromatico, mejor_sol, mejor_tiempo = min_colores_act, min_sol_act, min_tiempo_act
-
-    lavados = {}
-    for prenda_ in mejor_sol:
-        lavado_actual = mejor_sol[prenda_]
-        lavados[lavado_actual] = lavados.get(lavado_actual, []) + [prenda_]
-
-    mostrar_solucion_en_pantalla(lavados, tiempo_de_lavado)
-    escribir_solucion(lavados)
+    return sol_optima
 
 
-def coloreo_rec(grafo, k, v_pintados, v, min_colores, min_sol, min_tiempo, tiempo_lavados):
-    for color in range(1, k + 1):
-        v_pintados[v] = color  # Pinto al vertice v del color `color`
+def coloreo_rec(dominio, sol_parcial, v, sol_optima):
+    # Para cada color, pinto el vertice de ese color, y, si es compatible, me fijo si me da menor tiempo que antes
+    for color in range(1, sol_optima[MEJOR_IND_CROMATICO] + 1):
+        sol_parcial[PINTADOS][v] = color                        # Pinto al vertice v del color `color`
 
-        if not es_compatible(grafo, v_pintados, v):
+        if not es_compatible(dominio[GRAFO], sol_parcial[PINTADOS], v):
             continue
 
-        if usa_mas_colores(v_pintados, min_colores):
+        t_lav_ant = sol_parcial[T_LAVADOS].get(color, 0)
+        sol_parcial[T_LAVADOS][color] = max(t_lav_ant, dominio[T_LAV_PRENDAS][v])
+        diferencia = dominio[T_LAV_PRENDAS][v] - t_lav_ant
+        if diferencia > 0:
+            sol_parcial[T_TOTAL_LAVADOS] += diferencia
+
+        if sol_parcial[T_TOTAL_LAVADOS] > sol_optima[MEJOR_TIEMPO]:
+            # Vuelvo a poner los tiempos de lavado como estaban antes
+            sol_parcial[T_LAVADOS][color] = t_lav_ant
+            sol_parcial[T_TOTAL_LAVADOS] -= diferencia
             continue
 
-        if usa_mas_tiempo_lavado(v_pintados, min_tiempo, tiempo_lavados):
-            continue
-
-        correcto = True
-        for w in grafo.neighbors(v):
-            if w in v_pintados:
+        sol_parcial[CORRECTO] = True
+        for w in dominio[GRAFO].neighbors(v):
+            if w in sol_parcial[PINTADOS]:
                 continue
 
-            min_colores, min_sol, min_tiempo, correcto = coloreo_rec(grafo, k,
-                                                                     v_pintados, w,
-                                                                     min_colores, min_sol,
-                                                                     min_tiempo, tiempo_lavados)
-            if not correcto:
+            sol_parcial = coloreo_rec(dominio, sol_parcial, w, sol_optima)
+
+            if not sol_parcial[CORRECTO] or len(sol_parcial[PINTADOS]) == len(sol_optima[MEJOR_SOL]):
                 break
 
-        if correcto:
-            if len(v_pintados) > len(min_sol):
-                min_sol = v_pintados
-            if len(v_pintados) == grafo.number_of_nodes() and not usa_mas_colores(v_pintados, min_colores):
-                return len(v_pintados), v_pintados, obtener_tiempo_lavado(v_pintados, tiempo_lavados), True
-            return min_colores, min_sol, min_tiempo, True
+        if sol_parcial[CORRECTO]:
+            # if (len(sol_parcial[PINTADOS]) >= len(sol_optima[MEJOR_SOL]) and
+            # sol_parcial[PINTADOS] != sol_optima[MEJOR_SOL] and
+            if(
+                    sol_parcial[T_TOTAL_LAVADOS] < sol_optima[MEJOR_TIEMPO]):
+                return sol_parcial
 
-    del v_pintados[v]
-    return min_colores, min_sol, min_tiempo, False
+        # Vuelvo a poner los tiempos de lavado como estaban antes
+        sol_parcial[T_LAVADOS][color] = t_lav_ant
+        sol_parcial[T_TOTAL_LAVADOS] -= diferencia
+
+    del sol_parcial[PINTADOS][v]
+    sol_parcial[CORRECTO] = False
+    return sol_parcial
 
 
 if __name__ == "__main__":
-    prendas, d_incompatibilidades, d_lavados = procesar_archivo_prendas()
-    grafo_prendas = crear_grafo(prendas, d_incompatibilidades)
-    solucion_por_coloreo(d_lavados)
+    cantidad_prendas, dic_incompatibilidades, dic_lav_prendas = procesar_archivo_prendas()
+    grafo_prendas = crear_grafo(cantidad_prendas, dic_incompatibilidades)
+    solucion = solucion_por_coloreo(cantidad_prendas, dic_lav_prendas)
+
+    lavados_solucion = armado_lavados(solucion[MEJOR_SOL])
+    mostrar_solucion_en_pantalla(lavados_solucion, dic_lav_prendas)
+    escribir_solucion(lavados_solucion)
